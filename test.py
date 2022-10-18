dc28dc3b9ec52163149a3e73766e5e1f1179d68f
Lamminhtuan <tuanlam300@gmail.com>
2022-10-18 12:14:26 +0700

    1

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-from sklearn.preprocessing import StandardScaler
-data = [[1,2], [3,4]]
-scaler = StandardScaler()
-scaler.fit(data)
-print(scaler.mean())
+from sklearn.preprocessing import Normalizer
+x = [[-4,1,2,2],[1,3,9,3]]
+transformer = Normalizer(norm = 'l1').fit(x)
+print(transformer.transform(x))
