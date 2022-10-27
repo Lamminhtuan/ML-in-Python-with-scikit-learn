ce90c6b8407128eba4bbcd3ced9ca66443118631
Lamminhtuan <tuanlam300@gmail.com>
2022-10-27 14:53:04 +0700

    update

--- regression.py
+++ regression.py
@@ -4,6 +4,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
 features = []
 uploaded_file = st.file_uploader("Chọn file:")
 if uploaded_file is not None:
@@ -18,13 +20,27 @@ if uploaded_file is not None:
     X = df[features]
     y = df[df.columns[-1]]
     st.write("Output: ", df.columns[-1])
+    for i in features:
+            if pd.to_numeric(X[i], errors='coerce').notnull().all() == False:
+                one_hot = pd.get_dummies(X[i])
+                X = X.drop(i, axis=1)
+                X = X.join(one_hot)
     split = st.number_input("Nhập hệ số chia train và test: ", min_value = 0.1, max_value = 1.0)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = split)
-    if st.checkbox("KFold: "):
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.fit_transform(X_test)
+    usekfold = st.checkbox("KFold: ")
+    if usekfold:
         number = st.number_input("Chọn hệ số k: ", min_value =2, format="%d")
     if st.button("Run"):
-        st.write('Waiting')
         
+        reg = LinearRegression()
+        reg.fit(X_train, y_train)
+        y_pred = reg.predict(X_test)
+        st.write('Mean squared error: ', mean_squared_error(y_test, y_pred))
+
+            
 
     
     
--- test.py
+++ test.py
@@ -1,3 +1,21 @@
+import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
 df = pd.read_csv('dataset/50_Startups.csv')
-print(len(df.columns[:-1]))
+features = df.columns[:-1]
+X = df[features]
+y = df[df.columns[-1]]
+for i in features:
+  if pd.to_numeric(X[i], errors='coerce').notnull().all() == False:
+      one_hot = pd.get_dummies(X[i])
+      X = X.drop(i, axis=1)
+      X = X.join(one_hot)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.8)
+scaler = StandardScaler()
+scaler.fit(X_train)
+scaler.fit(X_test)
+print(X_train)
