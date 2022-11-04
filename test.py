5f9c74e4d7313dc980e390f11995a3ee38369153
Lamminhtuan <tuanlam300@gmail.com>
2022-11-04 11:16:02 +0700

    sda

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import log_loss
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 df = pd.read_csv('datasets/Social_Network_Ads.csv')
 X = df.iloc[:, :-1]
@@ -12,4 +14,6 @@ reg = LogisticRegression()
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
 reg.fit(X_train, y_train)
 y_pred = reg.predict(X_test)
-print(f1_score(y_test, y_pred, zero_division=1))
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(f1_score(y_test, y_pred))
